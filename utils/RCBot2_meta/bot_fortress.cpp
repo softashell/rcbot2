4189acaf1163996436b6bb503f750635a6d04f56
Ryan Hunter <robocop@lycos.co.uk>
2025-01-06 20:53:38 +0000

    TF2 Sniper and Projectile update by pongo1231
    https://github.com/pongo1231/rcbot2/commit/e5a1fc6c1db44ce106a021d180860249d9588b80
    
    https: //github.com/pongo1231/rcbot2/commit/0143617a0cbc0d80b785fa5734b415fd701f0042#diff-99422747195ed6b65796730c9e98757541b6a6c5664a63973be28ba4ae6fe056R7302
    Co-Authored-By: pongo1231 <pongo12310@gmail.com>

--- utils/RCBot2_meta/bot_fortress.cpp
+++ utils/RCBot2_meta/bot_fortress.cpp
@@ -4804,7 +4804,10 @@ void CBotTF2 :: getTasks ( unsigned iIgnore )
 		(m_fLastKnownTeamFlagTime && (m_fLastKnownTeamFlagTime > engine->Time())), 
 		fDefendFlagUtility + (randomFloat (0.0f, 0.2f) - 0.1f))
 
-	ADD_UTILITY(BOT_UTIL_SNIPE, (iClass == TF_CLASS_SNIPER) && m_pWeapons->hasWeapon(TF2_WEAPON_SNIPERRIFLE) && !m_pWeapons->getWeapon(CWeapons::getWeapon(TF2_WEAPON_SNIPERRIFLE))->outOfAmmo(this) && !hasSomeConditions(CONDITION_PARANOID) && !bHasFlag && (getHealthPercent()>0.2f), 0.95f)
+	ADD_UTILITY(BOT_UTIL_SNIPE, (iClass == TF_CLASS_SNIPER) && m_pWeapons->getCurrentWeaponInSlot(0)
+		&& !m_pWeapons->getCurrentWeaponInSlot(0)->isProjectile()
+		&& !m_pWeapons->getCurrentWeaponInSlot(0)->outOfAmmo(this) && !hasSomeConditions(CONDITION_PARANOID)
+		&& !bHasFlag && (getHealthPercent() > 0.2f), 0.95f);
 	//ADD_UTILITY(BOT_UTIL_SNIPE_CROSSBOW, (iClass == TF_CLASS_SNIPER) && m_pWeapons->hasWeapon(TF2_WEAPON_BOW) && !m_pWeapons->getWeapon(CWeapons::getWeapon(TF2_WEAPON_BOW))->outOfAmmo(this) && !hasSomeConditions(CONDITION_PARANOID) && !bHasFlag && (getHealthPercent()>0.2f), 0.95f)
 
 	ADD_UTILITY(BOT_UTIL_ROAM,true,0.0001f)
@@ -7158,7 +7161,7 @@ bool CBotTF2 :: handleAttack ( CBotWeapon *pWeapon, edict_t *pEnemy )
 				return false; // don't attack the rocket anymore
 		}
 
-		if ((m_iClass == TF_CLASS_SNIPER) && (pWeapon->getID() == TF2_WEAPON_BOW))
+		if (m_iClass == TF_CLASS_SNIPER && pWeapon->isProjectile())
 		{
 			//stopMoving();
 
@@ -7177,7 +7180,7 @@ bool CBotTF2 :: handleAttack ( CBotWeapon *pWeapon, edict_t *pEnemy )
 				m_pButtons->letGo(IN_ATTACK);
 			}
 		}
-		else if ( (m_iClass == TF_CLASS_SNIPER) && (pWeapon->getID() == TF2_WEAPON_SNIPERRIFLE) ) 
+		else if (m_iClass == TF_CLASS_SNIPER && pWeapon->getWeaponInfo() && pWeapon->getWeaponInfo()->getSlot() == 0)
 		{
 			//stopMoving();
 
